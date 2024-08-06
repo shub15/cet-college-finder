dcaf2fc6bb77ff219e43ff35aa8e7eca3fe6afb5
Shub15 <maityshubham1510@gmail.com>
2024-08-06 21:15:07 +0530

    body text size change for mobile devices

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,8 +4,8 @@ import Form from './Form'
 export default function Body() {
   return (
     <div className='bg-blue-500'>
-      <div className='p-10 text-center bg-blue-950 '>
-        <p className='m-10 text-5xl font-bold text-white'>
+      <div className='p-5 sm:p-10 text-center bg-blue-950 '>
+        <p className='m-5 sm:m-10 sm:text-5xl text-2xl font-bold text-white'>
           This website can help to find best college
         </p>
       </div>
