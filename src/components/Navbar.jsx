e492d530368a4db65cef31afbd34a80ea557f478
Shub15 <maityshubham1510@gmail.com>
2024-08-16 13:03:37 +0530

    Bug fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -87,18 +87,18 @@ export default function Navbar(props) {
             <DisclosurePanel className="sm:hidden">
                 <div className="space-y-1 px-2 pb-3 pt-2">
                     {navigation.map((item) => (
-                        <DisclosureButton
+                        <NavLink
                             key={item.name}
                             as="a"
-                            href={item.href}
+                            to={item.href}
                             aria-current={item.current ? 'page' : undefined}
-                            className={classNames(
-                                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                            className={({ isActive }) => classNames(
+                                isActive ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                                 'block rounded-md px-3 py-2 text-base font-medium',
                             )}
                         >
                             {item.name}
-                        </DisclosureButton>
+                        </NavLink>
                     ))}
                 </div>
             </DisclosurePanel>
