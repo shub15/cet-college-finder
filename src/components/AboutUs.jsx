b1bdaa84cfc7b053131f50f2b829aa94586ba517
Shub15 <maityshubham1510@gmail.com>
2024-11-08 02:19:41 +0530

    improved frontend

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -51,7 +51,7 @@ const AboutUs = () => {
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
                         {/* Team Member 1 */}
                         <div className="bg-white shadow-md p-6 rounded-lg text-center">
-                            <img src="https://via.placeholder.com/150" alt="Team Member" className="w-32 h-32 mx-auto rounded-full mb-4" />
+                            <img src="https://scontent-bom2-2.cdninstagram.com/v/t51.2885-19/461460543_1276564690004495_1551109590864935576_n.jpg?stp=dst-jpg_s150x150&_nc_cat=110&ccb=1-7&_nc_sid=f7ccc5&_nc_ohc=cKqnqU0Y_o8Q7kNvgEruA9L&_nc_ad=z-m&_nc_cid=0&_nc_zt=24&_nc_ht=scontent-bom2-2.cdninstagram.com&oh=00_AYBbhBLZdHHCmoRJtRU5qgiw4NbfCqh3_WRSP8A0xMmXHg&oe=67330FA7" alt="Team Member" className="w-32 h-32 mx-auto rounded-full mb-4" />
                             <h3 className="text-xl font-semibold text-gray-800">Sahil Doshi</h3>
                             <p className="text-blue-600">Founder</p>
                             <p className="text-gray-600 mt-2">
@@ -61,7 +61,7 @@ const AboutUs = () => {
 
                         {/* Team Member 2 */}
                         <div className="bg-white shadow-md p-6 rounded-lg text-center">
-                            <img src="https://via.placeholder.com/150" alt="Team Member" className="w-32 h-32 mx-auto rounded-full mb-4" />
+                            <img src="https://avatars.githubusercontent.com/u/83867127?v=4" alt="Team Member" className="w-32 h-32 mx-auto rounded-full mb-4" />
                             <h3 className="text-xl font-semibold text-gray-800">Shubham Maity</h3>
                             <p className="text-blue-600">Project Lead</p>
                             <p className="text-gray-600 mt-2">
@@ -71,7 +71,7 @@ const AboutUs = () => {
 
                         {/* Team Member 3 */}
                         <div className="bg-white shadow-md p-6 rounded-lg text-center">
-                            <img src="https://via.placeholder.com/150" alt="Team Member" className="w-32 h-32 mx-auto rounded-full mb-4" />
+                            <img src="https://scontent-bom1-2.cdninstagram.com/v/t51.2885-19/448179061_428419473399688_8658991707102580644_n.jpg?stp=dst-jpg_s100x100&_nc_cat=100&ccb=1-7&_nc_sid=bf7eb4&_nc_ohc=VwTHwpEe1EUQ7kNvgGAOFEe&_nc_zt=24&_nc_ht=scontent-bom1-2.cdninstagram.com&oh=00_AYBwaxcU-Czqihfx-NEtVtn7X2oSAeAe6SAsi-RQjpuEcA&oe=6732F306" alt="Team Member" className="w-32 h-32 mx-auto rounded-full mb-4" />
                             <h3 className="text-xl font-semibold text-gray-800">Vatsal Umrania</h3>
                             <p className="text-blue-600">Data Base</p>
                             <p className="text-gray-600 mt-2">
